ccf99a1d96ac9bfe17e49c8ac6ba2a3f868617b6
rock1nbvv <vladvlad03072000@gmail.com>
2025-11-13 03:08:41 +0000

    3228. Maximum Number of Operations to Move Ones to the End #update 13-11-2025

--- src/main/java/leetcode/MaximumNumberOfOperationsToMoveOnesToTheEnd3228.java
+++ src/main/java/leetcode/MaximumNumberOfOperationsToMoveOnesToTheEnd3228.java
@@ -3,16 +3,20 @@ package leetcode;
 /**
  * <a href="https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/">3228. Maximum Number of Operations to Move Ones to the End</a>
  * <br>
- * NOTE: good to know how chars interact. Go through an array, count ones, if we find a case when s[i]=0 and s[i-1]=1 we increment amount of operations by the amount of already counted ones
+ * NOTE: good to know how chars interact. Go through an array, remember each one, and when finding a zero add all counted ones to the result and jump to the next 1
  */
 public class MaximumNumberOfOperationsToMoveOnesToTheEnd3228 {
     public static int maxOperations(String s) {
         int ops = 0;
         int ones = 0;
         for (int i = 0; i < s.length(); i++) {
-            ones += s.charAt(i) - '0';
-            if (i > 0 && s.charAt(i) < s.charAt(i - 1)) {
+            if(s.charAt(i) =='1'){
+                ones ++;
+            }else if(s.charAt(i)=='0'){
                 ops+=ones;
+                while(i<s.length()-1 && s.charAt(i+1)=='0'){
+                    i++;
+                }
             }
         }
         return ops;
